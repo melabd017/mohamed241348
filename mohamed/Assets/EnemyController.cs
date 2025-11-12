809475428c25fb05ae8a134a2fe2a3ca16ebb8a0
melabd017 <melabd017@gmail.com>
2025-11-12 05:20:51 -0800

    lab 5 solutions

---
+++ mohamed/Assets/EnemyController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float maxSpeed = 2;
+    public int damage = 1;
+    public SpriteRenderer sr;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+
+    public void Flip()
+    {
+        sr.flipX = !sr.flipX;
+    }
+
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<PlayerStats>().TakeDamage(damage);
+            Flip();
+        }
+        else if (other.tag == "Wall")
+        {
+            Flip();
+        }
+    }
+}
